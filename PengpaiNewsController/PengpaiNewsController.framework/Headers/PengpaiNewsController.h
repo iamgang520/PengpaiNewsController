186cf4446a0b8b1457292ec2024eb58a98cc1fe3
iamgang <5391519@qq.com>
2018-10-17 16:04:26 +0800

    创建版本 0.0.1

---
+++ PengpaiNewsController/PengpaiNewsController.framework/Headers/PengpaiNewsController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PengpaiNewsController
+//
+//  Created by iamgang on 2018/10/17.
+//  Copyright © 2018年 iamgang. All rights reserved.
+//
+
+#import <TMWebFramework/TMWebViewController.h>
+
+@interface ViewController : TMWebViewController
+
+
+@end
+
